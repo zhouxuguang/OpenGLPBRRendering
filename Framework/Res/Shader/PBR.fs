2be8b7d387ba8d137d4e73f31fe694056d862e55
zhouxuguang <zhouxuguang236@126.com>
2025-06-22 13:14:32 +0800

    直接光的D的实现

--- Framework/Res/Shader/PBR.fs
+++ Framework/Res/Shader/PBR.fs
@@ -10,11 +10,22 @@ uniform DefaultVec4s
     vec4 Reserved[1023];
 };
 
+const float PI = 3.1415926;
+
 out vec4 OutColor0;
 
 vec3 Fresnel(in vec3 inF0, in float HdotV)
 {
     return inF0 + (vec3(1.0) - inF0) * pow(1-HdotV, 5.0);
+}
+
+float NDF(float NdotH, float inRoughness)
+{
+    float roughness = inRoughness * inRoughness;
+    float numerator = roughness * roughness;
+    float denominator = (NdotH * NdotH) * (numerator - 1.0) + 1.0;
+    denominator = PI * denominator * denominator;
+    return numerator / denominator;
 }
 
 void main()
@@ -25,12 +36,16 @@ void main()
     vec3 H = normalize(L+V);
     float NdotL = max(dot(N, L), 0.0);
     float HdotV = max(dot(H, V), 0.0);
+    float NdotH = max(dot(N, H), 0.0);
+
+    float inRoughness = 1.0;
 
     vec3 FinalColor = vec3(0.0);
     //direct light
     {
         vec3 Ks = Fresnel(vec3(0.04), HdotV);
-        vec3 specular = Ks;//vec3(0.0);
+        float D = NDF(NdotH, inRoughness);
+        vec3 specular = D * Ks;
         vec3 diffuse = vec3(0.0);
         FinalColor = (diffuse + specular) * NdotL;
     }
